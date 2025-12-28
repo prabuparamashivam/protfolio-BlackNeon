ba6a69c900218b68cb0556d7373184291ad40925
prabu paramashivam <prabuparamashivam@gmail.com>
2025-12-28 20:48:05 +0530

    resume added

--- NewProtfolio/src/components/ProfileSection.jsx
+++ NewProtfolio/src/components/ProfileSection.jsx
@@ -3,10 +3,34 @@ import { LanguagesBar } from "./LanguagesBar";
 
 export default function ProfileSection() {
   return (
-    <section className="bg-black px-4 md:px-12 py-15">
+    <section className="bg-black px-4 md:px-12 py-16">
       <div className="max-w-6xl mx-auto space-y-6">
+
         <Awards />
         <LanguagesBar />
+
+        {/* Download Resume Button */}
+        <div className="flex justify-center pt-4">
+          <a
+            href="/resume/LINGAPRABHU_S_RESUME.pdf"
+            download
+            className="
+              inline-flex items-center justify-center
+              px-8 py-3
+              rounded-full
+              bg-[#e4f65a]
+              text-black
+              font-semibold
+              text-sm
+              shadow-[0_0_20px_rgba(57,255,20,0.6)]
+              hover:shadow-[0_0_30px_rgba(57,255,20,0.9)]
+              transition
+            "
+          >
+            Download Resume
+          </a>
+        </div>
+
       </div>
     </section>
   );
